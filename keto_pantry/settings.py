7fbf73f0b925bbcbc8c0fe11b394ddd7fcbcecd4
Holly Horwood <holly.horwood@live.com>
2019-09-16 13:17:11 +1200

    added URL if statement

--- keto_pantry/settings.py
+++ keto_pantry/settings.py
@@ -12,8 +12,8 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 
-if os.path.exists('env.py'):
-	import env
+""" if os.path.exists('env.py'):
+	import env """
 
 import dj_database_url    
 
@@ -86,19 +86,16 @@ TEMPLATES = [
 WSGI_APPLICATION = 'keto_pantry.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
-DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-}
+if "DATABASE_URL" in os.environ:
+    DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
+else:
+    print("Database URL not found. Using SQLite instead")
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
