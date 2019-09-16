79c286f153d3eb32d362892ee39e5e6a5dc7e9a9
Holly Horwood <holly.horwood@live.com>
2019-09-16 16:43:37 +1200

    testing email settings in Heroku

--- keto_pantry/settings.py
+++ keto_pantry/settings.py
@@ -175,9 +175,8 @@ AUTHENTICATION_BACKENDS = [
 ]
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_USE_SSL = True
-EMAIL_USE_TLS = False
-EMAIL_PORT = 465
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = os.environ.get("EMAIL_ADDRESS") 
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PW")
