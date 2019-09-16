a1f8419d672a0aaa025ba9467a334415ac3c4357
Holly Horwood <holly.horwood@live.com>
2019-09-16 16:33:34 +1200

    configured email settings not working locally but working via Heroku

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 Pillows installed to view images in Django
 
 
-Travis was used for continuous integrated testing
+Travis was used for continuous integration testing, checks all packages are valid and project compiles. Once configured all builds are passing.
 [![Build Status](https://travis-ci.org/Holly-Horwood/keto_pantry.svg?branch=master)](https://travis-ci.org/Holly-Horwood/keto_pantry)
 
 Testing
--- keto_pantry/settings.py
+++ keto_pantry/settings.py
@@ -169,21 +169,21 @@ STRIPE_SECRET = os.getenv('STRIPE_SECRET')
 
 MESSAGE_STORAGE = "django.contrib.messages.storage.session.SessionStorage"
 
-"""EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"""
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
     'accounts.backends.EmailAuth'
 ]
 
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_SSL = True
+EMAIL_USE_TLS = False
+EMAIL_PORT = 465
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = "EMAIL_ADDRESS" 
-EMAIL_HOST_PASSWORD = "EMAIL_PW"
-""" EMAIL_USE_SSL = True
-EMAIL_PORT = 465 """
+EMAIL_HOST_USER = os.environ.get("EMAIL_ADDRESS") 
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PW")
+DEFAULT_FROM_EMAIL = os.environ.get("EMAIL_ADDRESS") 
+SERVER_EMAIL = os.environ.get("EMAIL_ADDRESS")
+
 
 
 
