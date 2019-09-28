86557b6b5567d0dd00bb1d276ff3802294094b29
Holly Horwood <holly.horwood@live.com>
2019-09-29 12:37:55 +1300

    added back in S3 Media URL code but still not working

--- keto_pantry/settings.py
+++ keto_pantry/settings.py
@@ -138,7 +138,10 @@ AWS_S3_REGION_NAME = 'ap-southeast-2'
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_SECRET_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
 
-AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.%s.amazonaws.com' % (AWS_STORAGE_BUCKET_NAME, AWS_S3_REGION_NAME)
+
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
 
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
@@ -153,7 +156,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
 
-MEDIA_URL = '/media/' 
+MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, '/media/images/') 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STRIPE_PUBLISHABLE = os.getenv('STRIPE_PUBLISHABLE')
