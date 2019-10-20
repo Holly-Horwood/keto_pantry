fad351d78762d2e801de2fb28a6fa6cb280d3e60
Holly Horwood <holly.horwood@live.com>
2019-10-21 09:14:11 +1300

    changed context.py back to customized code

--- cart/contexts.py
+++ cart/contexts.py
@@ -4,18 +4,17 @@ from .models import Cart
 
 #Ensures cart items are available on every page  
 
-def cart_contents(request):      
+def cart_contents(request):
+    cartDict = request.session.get('cart', {})
+    cart_items = []
+    total = 0
+    product_count = 0
+    if cartDict:
+        cart = Cart.objects.get(id=cartDict['id'])
+        for cart_line_item in cart.cartlineitem_set.all():
+            product = cart_line_item.product
+            total += cart_line_item.quantity * product.price
+            product_count += cart_line_item.quantity
+            cart_items.append({'id': cart_line_item.id, 'quantity': cart_line_item.quantity, 'product': product})
 
-    cart = request.session.get('cart', {})        
-    cart_items = []      
-    total = 0      
-    product_count = 0            
-
-    if request.user.is_authenticated:
-        for id, quantity in cart.items():          
-            product = get_object_or_404(Product, pk=product_id)          
-            total += quantity * product.price          
-            product_count += quantity          
-
-        cart_items.append({'id': id, 'quantity': quantity, 'product': product})            
     return {'cart_items': cart_items, 'total': total, 'product_count': product_count}
