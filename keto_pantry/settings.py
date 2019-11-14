74297b5f1988cc57febb7897c724c3e7ae8588c4
Holly Horwood <holly.horwood@live.com>
2019-11-15 06:01:27 +1300

    fixed aws secret access key variable that was wrong

--- keto_pantry/settings.py
+++ keto_pantry/settings.py
@@ -142,7 +142,7 @@ AWS_S3_OBJECT_PARAMETERS = {
 }
 
 AWS_STORAGE_BUCKET_NAME = "ketopantry"
-AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+AWS_ACCESS_KEY_ID = os.environ.get("AWS_SECRET_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
 AWS_S3_HOST = 's3-ap-southeast-2.amazonaws.com'
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
