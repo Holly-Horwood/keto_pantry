6d2c69f31d8e6f79724c68ad762576d59423e562
Holly Horwood <holly.horwood@live.com>
2020-02-17 09:54:08 +1300

    removed order app

--- order/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from cart.views import view_cart
-from .views import view_order
-
-urlpatterns = [
-    url(r'^$', view_order, name='view_order'),
-    url(r'^cart', view_cart, name='view_cart')
-]
